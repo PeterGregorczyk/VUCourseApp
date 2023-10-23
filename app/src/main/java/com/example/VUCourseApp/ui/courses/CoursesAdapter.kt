63b4ac84cfd295e645766a3366da9948332a5d30
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-10-23 16:09:00 +1100

    working searchView

--- app/src/main/java/com/example/VUCourseApp/ui/courses/CoursesAdapter.kt
+++ app/src/main/java/com/example/VUCourseApp/ui/courses/CoursesAdapter.kt
@@ -3,13 +3,18 @@ package com.example.VUCourseApp.ui.courses
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.VUCourseApp.data.model.Course
 import com.example.shitapp.R
+import java.util.Locale
 
 class CourseAdapter(private var data: List<Course>) :
-    RecyclerView.Adapter<CourseAdapter.CourseViewHolder>() {
+    RecyclerView.Adapter<CourseAdapter.CourseViewHolder>(), Filterable {
+
+    var dataFilterList: List<Course> = ArrayList()
     class CourseViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(info: Course) {
@@ -34,17 +39,49 @@ class CourseAdapter(private var data: List<Course>) :
 
     //this just gets the amount of objects from the course list
     override fun getItemCount(): Int {
-        return data.size
+        return dataFilterList.size
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
 
         //this binds the data to the positions designated earlier
-        holder.bind(data[position])
+        holder.bind(dataFilterList[position])
+    }
+
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    dataFilterList = data
+                } else {
+                    val resultList = ArrayList<Course>()
+                    for (row in data) {
+                        if (row.courseTitle.lowercase(Locale.ROOT)
+                                .contains(charSearch.lowercase(Locale.ROOT))
+                        ) {
+                            resultList.add(row)
+                        }
+                    }
+                    dataFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = dataFilterList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                dataFilterList = results?.values as ArrayList<Course>
+                notifyDataSetChanged()
+            }
+        }
     }
 
     fun addData(newData: List<Course>) {
         data = newData as ArrayList<Course>
+        dataFilterList = data
         notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/VUCourseApp/ui/courses/CoursesFragment.kt
+++ app/src/main/java/com/example/VUCourseApp/ui/courses/CoursesFragment.kt
@@ -1,5 +1,4 @@
 package com.example.VUCourseApp.ui.courses
-
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -17,53 +16,55 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-
 class CoursesFragment : Fragment() {
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
+            .baseUrl("https://5ceb-106-71-104-5.ngrok-free.app") // Add the correct base url
             .baseUrl("https://0e0a-106-71-104-5.ngrok-free.app") // Add the correct base url
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
-
     private val courseApi: CourseAPI by lazy {
         retrofit.create(CourseAPI::class.java)
     }
-
     private var _binding: FragmentCoursesBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         _binding = FragmentCoursesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
-
+        val searchText = root.findViewById<SearchView>(R.id.TextSearch)
         val recyclerView = root.findViewById<RecyclerView>(R.id.coursesRecycler)
         recyclerView.layoutManager = LinearLayoutManager(this.context)
 
         val adapter = CourseAdapter(emptyList())
         recyclerView.adapter = adapter
-
         CoroutineScope(Dispatchers.Main).launch {
             try {
                 val courses = courseApi.getCourses().courses
-               adapter.addData(courses)
+                searchText.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        adapter.filter.filter(newText)
+                        return false
+                    }
+                })
+                adapter.addData(courses)
                 Log.e("CoursesFragment", "got courses ${courses}")
             } catch (e: Exception) {
                 Log.e("CoursesFragment", "Error fetching courses: ${e.message}")
             }
         }
-
         return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
